d90785cca7b808b019816e6ab92ff6088911aa4c
honeydev0820 <honeysoft0103@gmail.com>
2022-09-15 21:04:10 +0100

    Added command line installation feature

--- database/migrations/2022_09_06_172335_add_unique_key_to_my_classes_table.php
+++ database/migrations/2022_09_06_172335_add_unique_key_to_my_classes_table.php
@@ -4,19 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class() extends Migration {
     public function up(): void
     {
         Schema::table('my_classes', static function (Blueprint $table) {
-            $table->unique(['class_Group_id', 'name']);
+            $table->unique(['class_group_id', 'name']);
         });
     }
 
     public function down(): void
     {
         Schema::table('my_classes', static function (Blueprint $table) {
-            $table->dropUnique('my_classes_class_group_id_name_unique');
+            //it's not what it looks like, please dont remove the drop foreign
+            $table->dropForeign(['class_group_id']);
+            $table->dropUnique("my_classes_class_group_id_name_unique");
         });
     }
 };
